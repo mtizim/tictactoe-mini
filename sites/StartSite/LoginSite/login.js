3fa1559dc0250791e48d9f5a0c1a51b9a642e09e
ARUMHC <56126275+ARUMHC@users.noreply.github.com>
2022-01-23 20:27:51 +0100

    add registering and login in

---
+++ sites/StartSite/LoginSite/login.js
@@ -0,0 +1,32 @@
+//czyli 
+
+async function doLoginForm() {
+  // (B1) FORM DATA OBJECT
+  var logindata = new FormData();
+  logindata.append(
+    "username",
+    document.getElementById("username_log").value
+  );
+  logindata.append(
+    "password",
+    document.getElementById("password_log").value
+  );
+ var username = logindata.get("username");
+ var password = logindata.get("password");
+
+login_string = `grant_type=&username=${username}&password=${password}&client_id=&client_secret=`;
+console.log(login_string);
+  //POST token
+  const login_response = await fetch("http://localhost:8000/token", {
+    method: "POST",
+    body: login_string, // string or object
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  const login_responsejsonlog = await login_response.json(); //extract JSON from the http response
+  console.log(login_responsejsonlog);
+
+  return false;
+}
--- sites/StartSite/RegisterSite/register.js
+++ sites/StartSite/RegisterSite/register.js
@@ -12,11 +12,6 @@ async function doRegisterForm() {
     document.getElementById("password_col").value
   );
 
-  // wypisać
-  for (let [k, v] of registerdata.entries()) {
-    console.log(k, v);
-  }
-
   var object = {};
   registerdata.forEach(function (v, k) {
     object[k] = v;
@@ -35,7 +30,26 @@ async function doRegisterForm() {
   });
   const responsemyJson = await response.json(); //extract JSON from the http response
   console.log(responsemyJson);
-  // do something with myJson
+  console.log("registered");
+ 
+
+ //silent loging in
+var username = registerdata.get("username");
+var password = registerdata.get("password");
+
+login_string = `grant_type=&username=${username}&password=${password}&client_id=&client_secret=`;
+console.log(login_string);
+  //POST token
+  const login_response = await fetch("http://localhost:8000/token", {
+    method: "POST",
+    body: login_string, // string or object
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  const login_responsejsonlog = await login_response.json(); //extract JSON from the http response
+  console.log(login_responsejsonlog);
 
   return false;
 }
