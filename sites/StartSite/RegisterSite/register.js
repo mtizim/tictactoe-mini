f31e8e51c120f43ede241ada8a5adb802b862d24
Mateusz Ziemła <mtizim@pm.me>
2022-01-23 16:24:20 +0100

    CORS fix 3

--- sites/StartSite/RegisterSite/register.js
+++ sites/StartSite/RegisterSite/register.js
@@ -1,38 +1,41 @@
-
-
-function doRegisterForm() {
+async function doRegisterForm() {
   // (B1) FORM DATA OBJECT
   var registerdata = new FormData();
 
   // (B2) APPEND FIELDS
-  registerdata.append("username", document.getElementById("username_col").value);
-  registerdata.append("password", document.getElementById("password_col").value);
+  registerdata.append(
+    "username",
+    document.getElementById("username_col").value
+  );
+  registerdata.append(
+    "password",
+    document.getElementById("password_col").value
+  );
 
   // wypisać
-  for (let [k, v] of registerdata.entries()) { console.log(k, v); }
-  
+  for (let [k, v] of registerdata.entries()) {
+    console.log(k, v);
+  }
+
   var object = {};
-  registerdata.forEach(function(v, k){
+  registerdata.forEach(function (v, k) {
     object[k] = v;
   });
   var registerjson = JSON.stringify(object);
   //dla testu
   console.log(registerjson);
 
-  const userAction = async () => {
-  const response = await fetch('http://localhost:8000/register', {
-    method: 'POST',
+  const response = await fetch("http://localhost:8000/register", {
+    method: "POST",
     body: registerjson, // string or object
     headers: {
-      'Content-Type': 'application/json'
-    }
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
   });
   const responsemyJson = await response.json(); //extract JSON from the http response
   console.log(responsemyJson);
   // do something with myJson
-}
 
-
-  
   return false;
 }
