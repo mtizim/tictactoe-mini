8577984a1363c9e150cb1d6ed0639b7c0594c0ab
ARUMHC <56126275+ARUMHC@users.noreply.github.com>
2022-01-28 03:48:22 +0100

    room creating fix

--- sites/StartSite/RoomSite/roomcreating.js
+++ sites/StartSite/RoomSite/roomcreating.js
@@ -1,9 +1,8 @@
-//czyli 
+
 
 async function doCreateRoom() {
 
   var host = "https://tictactoe-mini.herokuapp.com/";
-  // (B1) FORM DATA OBJECT
   var roomdata = new FormData();
   roomdata.append(
     "room_id",
@@ -12,16 +11,16 @@ async function doCreateRoom() {
   
   //circle or cross
   var sign =  document.querySelector('input[name="sign"]:checked').value;
-  
+  localStorage.setItem("sign", sign);
+
   var object = {};
   roomdata.forEach(function (v, k) {
     object[k] = v;
   });
   var roomjson = JSON.stringify(object);
-
-
 console.log(object.room_id);
-var token = window.localStorage.getItem("player_token");
+
+var token = localStorage.getItem("player_token");
 console.log(token);
   //POST new room
   const room_response = await fetch(`${host}room/${object.room_id}`, {
@@ -34,6 +33,10 @@ console.log(token);
   });
   const roomresponsejson = await room_response.json(); //extract JSON from the http response
   console.log(roomresponsejson);
+
+if(roomresponsejson != null){alert(roomresponsejson.detail);}else{
+    window.location.href='../GameSite/GameSite.html';
+  }
 
   return false;
 }
